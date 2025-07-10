6461a65b838c850e70e7277099fdcaa1a2bbcfb3
Vladimir Jucov <jucov@live.com>
2025-07-10 14:39:08 +0300

    add sanity files

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -1,4 +1,5 @@
 import "@/app/globals.css";
+import {SanityLive} from "@/sanity/lib/live";
 
 export default function RootLayout({
                                        children,
@@ -7,7 +8,10 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
-        <body>{children}</body>
+        <body>
+        {children}
+        <SanityLive />
+        </body>
         </html>
     );
 }
