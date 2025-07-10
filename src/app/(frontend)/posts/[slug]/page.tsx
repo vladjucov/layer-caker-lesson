1e2404dc7dda5a2d3e37bc72d6ff2848d6a61bf8
Vladimir Jucov <jucov@live.com>
2025-07-10 15:17:21 +0300

    add image handling and validation for post type

--- src/app/(frontend)/posts/[slug]/page.tsx
+++ src/app/(frontend)/posts/[slug]/page.tsx
@@ -2,6 +2,8 @@ import { sanityFetch } from "@/sanity/lib/live";
 import { POST_QUERY } from "@/sanity/lib/queries";
 import { notFound } from "next/navigation";
 import Link from "next/link";
+import { urlFor } from "@/sanity/lib/image";
+import Image from "next/image";
 
 export default async function Page({
                                        params,
@@ -19,6 +21,20 @@ export default async function Page({
 
     return (
         <main className="container mx-auto grid grid-cols-1 gap-6 p-12">
+            {post?.mainImage ? (
+                <Image
+                    className="w-full aspect-[800/300]"
+                    src={urlFor(post.mainImage)
+                        .width(800)
+                        .height(300)
+                        .quality(80)
+                        .auto("format")
+                        .url()}
+                    alt={post?.mainImage?.alt || ""}
+                    width="800"
+                    height="300"
+                />
+            ) : null}
             <h1 className="text-4xl font-bold text-balance">{post?.title}</h1>
             <hr />
             <Link href="/posts">&larr; Return to index</Link>
